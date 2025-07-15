8bd0f23afcab21f4a2d74d2daab99df280acb21d
Math012 <math111.0811@gmail.com>
2025-07-15 08:42:03 -0300

    Primeiro commit

---
+++ src/main/java/com/math012/crudpostgressql/CrudPostgressqlApplication.java
@@ -0,0 +1,13 @@
+package com.math012.crudpostgressql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPostgressqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPostgressqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/math012/crudpostgressql/CrudPostgressqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math012.crudpostgressql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPostgressqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
