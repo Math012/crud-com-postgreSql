ca842e7e09105a258c342309b4bcbda1b157d9f4
Math012 <math111.0811@gmail.com>
2025-07-16 14:08:19 -0300

    #Refatoração - Adicionando as anotações do lombok ao CategoriasResponseDTO

--- src/main/java/com/math012/crudpostgressql/business/DTO/response/CategoriasResponseDTO.java
+++ src/main/java/com/math012/crudpostgressql/business/DTO/response/CategoriasResponseDTO.java
@@ -1,9 +1,16 @@
 package com.math012.crudpostgressql.business.DTO.response;
 
+import lombok.*;
+
 import java.util.List;
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Builder
 public class CategoriasResponseDTO {
-
     private Long id;
     private String nome;
     private List<ProdutosResponseDTO> produtos;
