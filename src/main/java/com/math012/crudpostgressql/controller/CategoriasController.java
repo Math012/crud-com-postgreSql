bc8efa2a258001c824f589763c6012b5eeccc111
Math012 <math111.0811@gmail.com>
2025-07-17 20:22:11 -0300

    Adicionando CategoriasController

---
+++ src/main/java/com/math012/crudpostgressql/controller/CategoriasController.java
@@ -0,0 +1,28 @@
+package com.math012.crudpostgressql.controller;
+
+import com.math012.crudpostgressql.business.DTO.request.CategoriasRequestDTO;
+import com.math012.crudpostgressql.business.DTO.response.CategoriasResponseDTO;
+import com.math012.crudpostgressql.business.service.CategoriasService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/categorias")
+public class CategoriasController {
+
+    private final CategoriasService service;
+
+    @GetMapping
+    public ResponseEntity<List<CategoriasResponseDTO>> todasCategorias(){
+        return ResponseEntity.ok(service.listaProdutos());
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<CategoriasResponseDTO> criarCategoria(@RequestBody CategoriasRequestDTO categorias){
+        return ResponseEntity.ok(service.criarCategoria(categorias));
+    }
+}
